a2e7c15b4e39fa270757258dcc09db6dc1e186b2
anast-ananko <anast.ananko@gmail.com>
2021-08-08 15:52:53 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.join('').replace(/,/g, '');
+  let result = [];
+
+  for (let bracket of str) {
+      let index = brackets.indexOf(bracket);
+      if (index % 2 === 0) {
+          if (bracket === brackets[index + 1] && result[result.length - 1] === index){
+              result.pop();
+          } else if (bracket === brackets[index + 1] && result[result.length - 1] !== index) {
+              result.push(index);
+          }
+          else{
+              result.push(index);
+          }
+      } 
+      else {
+          if (result.pop() !== index-1){
+              return false;
+          }
+      }
+  }
+  return result.length === 0;
 }
